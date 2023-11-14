df65b984dcf932b56756f316d2f5209f69e8d981
hjlee <heunjong.lee@gmail.com>
2023-11-14 01:15:16 -0500

    set up express application with sequelize, handlebars, sessions

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express'); 
+const path = require('path'); // node module for paths
+
+// custom imports
+const routes = require('./controllers'); // controllers folder - routes
+const helpers = require('./utils/helpers'); 
+
+
+// express middleware for sessions and for handlebars
+const session = require('express-session'); 
+const exphbs = require('express-handlebars'); 
+
+
+// import sequelize, set up database connection 
+const sequelize = require('./config/connection'); 
+const SequelizeStore = require('connect-session-sequelize')(session.Store); 
+
+// create instance of express
+const app = express();
+
+const PORT = process.env.PORT || 3001; 
+
+// create instance of handlebars (using helpers)
+const hbs = exphbs.create({ helpers });
+
+
+const sess = {
+    // signs the session id cookie - should be encrypted, ideally
+    secret: 'kimnamjoonkimseokjinminyoongjunghosekparkjiminkimtaehyungjeonjungkookbts', 
+    cookie: {
+        maxAge: 300000, // max "age" of cookie - 300000 ms (5 min)
+        httpOnly: true, // restrict cookie access to HTTP requests
+        secure: false, // should set to true when deploying
+        sameSite: 'strict', // cookie sharing only for same-site requests
+    },
+    // resave false, save uninitialized true:
+    // save 'uninitialized sessions' (e.g. guest user), does not resave sessions with no new activity, 
+    // so there won't be duplicate identical sessions in the database
+    resave: false, // prevents session from being saved to the store if data is unmodified 
+    saveUninitialized: true, // stores session data even if it's 'uninitialized' - unmodified but new 
+    store: new SequelizeStore({
+        db: sequelize, // store session data in sequelize 
+    }),
+};
+
+
+app.use(session(sess));
+
+// set up handlebars view engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serve files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// use the defined routes
+app.use(routes);
+
+// syncing the models with the db and start app
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on port ' + PORT));
+});
