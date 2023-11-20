7b6cdcd3bec45b2031f8ae45eadb878219efa8a7
hjlee <heunjong.lee@gmail.com>
2023-11-20 14:13:02 -0500

    api routes configured

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
-const commentRoutes = require('./commentRoutes');
+const testRoutes = require('./testRoutes') // remove at deployment
+const userRoutes = require('./userRoutes')
+const tileRoutes = require('./tileRoutes')
+const trackerRoutes = require('./trackerRoutes')
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+
+
+router.use('/tests', testRoutes) // remove at deployment
+
+router.use('/users', userRoutes)
+router.use('/tiles', tileRoutes)
+router.use('/trackers', trackerRoutes)
+
 
 
 module.exports = router;
