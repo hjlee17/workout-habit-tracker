230df83c946a55ec6c158e08bba7111d7b6bb538
hjlee <heunjong.lee@gmail.com>
2023-11-21 17:07:31 -0500

    format_date helper modified to include time and local timezone in addition to date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,16 @@
 module.exports = {
     format_date: (date) => {
       if (date) {
-        return new Date(date).toLocaleDateString();
+        const options = {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+          hour12: true,
+          timeZoneName: 'short',
+        };
+        return new Date(date).toLocaleString('en-US', options);
       }
       return '';
     },
